d9dfc29fd006462bcdab3cee9d5df7ad2b92b76d
Luis Montaña <LuisMontana@users.noreply.github.com>
2022-03-10 18:45:49 -0500

    feat(GameManager): New Spawn Treasure feature

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int[] _pickupsToAdvance; // Amount of pickups needed to proceed to next "level"
     [SerializeField] private GameObject[] _levelObjects; // Objects that will be enabled as game advances
     [SerializeField] private float [] _timeToLose; // Objects that will be enabled as game advances
+    [SerializeField] private GameObject _treasurePrefab;
     private int _currentLevel;
     private float _timeRemaining;
     private float _cumulativeTime;
@@ -28,6 +29,7 @@ public class GameManager : MonoBehaviour
        _cumulativeTime = 0f;
        _timeRemaining = _timeToLose[_currentLevel];
        _currentPickups = 0;
+       InvokeRepeating("SpawnTreasures", 0f, 3f);
     }
 
     public void AddPickup() {
@@ -35,6 +37,18 @@ public class GameManager : MonoBehaviour
         if(_currentPickups == _pickupsToAdvance[_currentLevel]){
             _currentLevel++;
             _timeRemaining = _timeToLose[_currentLevel];
+        }
+    }
+
+    public void SpawnTreasures() {
+        bool spawned = false;
+        while(!spawned) {
+            Vector2 spawnPoint = new Vector2(Random.Range(-6, 6), Random.Range(-4.5f, 4.5f));
+            Collider2D collidingObject = Physics2D.OverlapCircle(spawnPoint, 1.5f);
+            if(collidingObject == false) {
+                Instantiate(_treasurePrefab, spawnPoint, Quaternion.identity);
+                spawned = true;
+            }
         }
     }
 
